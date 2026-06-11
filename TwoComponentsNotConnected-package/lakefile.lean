7620abb02bde5badd8b08441f7b6913ce93b767f
clemenskuske <33251410+clemenskuske@users.noreply.github.com>
2026-06-11 16:21:50 +0200

    Use statement package Mathlib dependency

--- TwoComponentsNotConnected-package/lakefile.lean
+++ TwoComponentsNotConnected-package/lakefile.lean
@@ -3,9 +3,6 @@ open Lake DSL
 
 package TwoComponentsNotConnected.Proofs where
 
-require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git" @ "c5ea00351c28e24afc9f0f84379aa41082b1188f"
-
 require TwoComponentsNotConnected.Statements from "./statement-package"
 
 @[default_target]
